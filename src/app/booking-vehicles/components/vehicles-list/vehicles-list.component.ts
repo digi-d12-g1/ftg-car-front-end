4757de3ee6b8c38640061557c2a2164ffccdb9ea
Tibo Pfeifer <tibo.pfeifer@gmail.com>
2023-02-09 17:07:20 +0100

    feat_booking: Logique métier choix dans la date fonctionnel

--- src/app/booking-vehicles/components/vehicles-list/vehicles-list.component.ts
+++ src/app/booking-vehicles/components/vehicles-list/vehicles-list.component.ts
@@ -8,17 +8,41 @@ import {NgbDateStruct, NgbTimeStruct} from "@ng-bootstrap/ng-bootstrap";
   templateUrl: './vehicles-list.component.html',
   styleUrls: ['./vehicles-list.component.scss']
 })
-export class VehiclesListComponent implements OnInit{
+export class VehiclesListComponent implements OnInit {
   vehicles: Vehicle[] = [];
+
   startDate!: NgbDateStruct;
   startTime!: NgbTimeStruct;
   endDate!: NgbDateStruct;
   endTime!: NgbTimeStruct;
+  dates: NgbDateStruct[] = []
 
-  constructor(private vehicleService: VehiclesWebService) {}
+  test: boolean = false
+
+  constructor(private vehicleService: VehiclesWebService) {
+  }
+  //https://medium.com/@andrey.shihov/ngb-bootstrap-datepickers-model-types-d76cdfe7e77d
 
   ngOnInit(): void {
     this.getVehicles();
+  }
+
+  checkDate() {
+    if(this.endDate === undefined) {
+      this.endDate = this.startDate
+    }
+    this.dates = [this.startDate, this.endDate]
+    console.log('Start Date : ' + JSON.stringify(this.startDate))
+    console.log('End Date : ' + JSON.stringify(this.endDate))
+  }
+  checkIfStartDateNotPassed(startDate: NgbDateStruct) {
+    let date = new Date();
+    return (
+      startDate.year < date.getUTCFullYear() ||
+      (startDate.year === date.getUTCFullYear() && startDate.month < date.getUTCMonth() + 1) ||
+      (startDate.year === date.getUTCFullYear() && startDate.month === date.getUTCMonth() + 1 &&
+        startDate.day < date.getUTCDay() + 5)
+    )
   }
 
   public getVehicles() {
