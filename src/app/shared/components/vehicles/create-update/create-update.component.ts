b3f7c90575d32b1bd30f4b4c5da62b34b9eacdf1
Romain <lemaire.romain37@gmail.com>
2023-02-10 10:15:33 +0100

    CRDU Update Vehicle

--- src/app/shared/components/vehicles/create-update/create-update.component.ts
+++ src/app/shared/components/vehicles/create-update/create-update.component.ts
@@ -102,7 +102,7 @@ updateVehicleGet() {
   ////////////////////////////////////////////// Update Vehicle POST ///////////////////////////////////////////////////
 
   updateVehiclePost(vehicle: Vehicle){
-    //vehicle.id = this.vehicleId // on rajoute l'id de l'objet reçu grâce au subject Behavior sinon pas d'id en venant du template
+    vehicle.id = this.vehicleId // on rajoute l'id de l'objet reçu grâce au subject Behavior sinon pas d'id en venant du template
     this.vehiclesWebService.updateVehicle(this.vehicleId , vehicle).subscribe();
     };
 
