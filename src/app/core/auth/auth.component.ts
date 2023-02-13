059c9165bbdee981183b71aafdbf386966c83a68
Romain <lemaire.romain37@gmail.com>
2023-02-13 14:01:25 +0100

    gitflow-feature-stash: authentification

--- src/app/core/auth/auth.component.ts
+++ src/app/core/auth/auth.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.scss']
 })
-export class AuthComponent {
+export class AuthComponent implements OnInit {
+
+  constructor( private authService: AuthService, private router: Router) {
+
+  }
+
+
+  ngOnInit(): void {
+  }
+
+  onLogin() {
+    this.authService.login();
+    this.router.navigateByUrl('/facesnaps');
+  }
 
 }
