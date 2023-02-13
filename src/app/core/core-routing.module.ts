debc46f7be6ea84fad2703acf5f6f442a7368437
Romain <lemaire.romain37@gmail.com>
2023-02-13 15:57:16 +0100

    Safe commit

--- src/app/core/core-routing.module.ts
+++ src/app/core/core-routing.module.ts
@@ -3,21 +3,22 @@ import {RouterModule, Routes} from '@angular/router';
 import {AdminPageComponent} from '../adminPage/components/admin-page/admin-page.component';
 import {VehiclesListComponent} from "../booking-vehicles/components/vehicles-list/vehicles-list.component";
 import {AuthComponent} from "./auth/components/auth.component";
+import { GuardComponent } from './auth/guard/guard.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: AuthComponent
-  },
   // {
-  //   path: 'auth/login',
+  //   path: '',
   //   component: AuthComponent
   // },
   {
+    path: 'auth/login',
+    component: AuthComponent
+  },
+  {
     path: 'admin',
     component: AdminPageComponent,
-    // canActivate: [GuardComponent],
+    canActivate: [GuardComponent],
     loadChildren: () => import('../adminPage/admin-page.module').then(m => m.AdminPageModule)
   },
   {
@@ -27,12 +28,12 @@ const routes: Routes = [
   },
   {
     path: 'profil',
-    // canActivate: [GuardComponent],
+    canActivate: [GuardComponent],
     loadChildren: () => import('../profil/profil.module').then(m => m.ProfilModule)
   },
   {
     path: '',
-    redirectTo: '',
+    redirectTo: 'auth/login',
     pathMatch: 'full'
   },
   {
