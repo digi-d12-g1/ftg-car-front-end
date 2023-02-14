84c5c684bf2b7e2dccd7e06cffb85d9143a2c638
Benours <56410867+Benours@users.noreply.github.com>
2023-02-14 12:13:26 +0100

    Add manipulation data

--- src/app/profil/components/carpooling/carpooling.component.ts
+++ src/app/profil/components/carpooling/carpooling.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AdvertCarpooling } from 'src/app/shared/models/advert-carpooling';
+import { AdvertCarpoolingWebService } from './../../../core/web-services/advert-carpooling.webservice';
 
 @Component({
   selector: 'app-carpooling',
@@ -6,5 +8,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./carpooling.component.scss']
 })
 export class CarpoolingComponent {
+  
+  advertCarpoolingList: AdvertCarpooling[] = [];
+  driverOrNot: boolean = false;
+  advertCarpooling: AdvertCarpooling = new AdvertCarpooling;
 
+    constructor(private advertCarpoolingWebService: AdvertCarpoolingWebService) {}
+
+    ngOnInit(): void {
+      this.AdvertgetAllCarpoolings();
+    }
+
+
+    // Get all advert carpooling
+    AdvertgetAllCarpoolings() {
+      this.advertCarpoolingWebService.getAllBookingWithIdEmployee(2).subscribe(data => {
+        this.advertCarpoolingList = data;
+      });
+    }
+
+    switchDrive() {
+      this.driverOrNot = true;
+    }
+
+    switchPassenger() {
+      this.driverOrNot = false;
+    }
+
+    getCarpooling(advert: AdvertCarpooling) {
+      this.advertCarpooling = advert;
+    }
 }
