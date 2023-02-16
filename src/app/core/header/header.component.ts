b12e6aee2479e3cb39b3e4e8b034c0fbe8b7370d
Romain <lemaire.romain37@gmail.com>
2023-02-16 08:55:10 +0100

    Mise en place isAdmin header

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,3 +1,4 @@
+import { Employee } from 'src/app/shared/models/employee';
 import { IsConnectService } from './../auth/services/isConect.service';
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
@@ -10,13 +11,21 @@ import { TokenStorageService } from '../auth/services/token-storage.service';
 })
 export class HeaderComponent implements OnInit {
 
-
+isAdmin: boolean = false;
+infoCoUser: Employee = new Employee;
 
   constructor(private router: Router, private tokenStorageService: TokenStorageService, private isConnectService: IsConnectService) {
 
   }
   ngOnInit(): void {
+    this.isUserAdmin();
+  }
 
+  private isUserAdmin() {
+    this.infoCoUser = this.tokenStorageService.getUser();
+    if (this.infoCoUser.isAdmin == true) {
+      this.isAdmin = true;
+    }
   }
 
   logOut():void {
